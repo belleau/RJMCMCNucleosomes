6a136d6ef22914b1009a3dffa323badc31f6ced7
belleau <pascal_belleau@hotmail.com>
2016-07-22 12:47:24 -0400

    Start folders structure

---
+++ src/configuration/SpaceState.cpp
@@ -0,0 +1,18 @@
+/*
+ * SpaceState.cpp
+ *
+ *  Created on: Jul 22, 2016
+ *      Author: belleau
+ */
+
+#include "SpaceState.h"
+
+SpaceState::SpaceState() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SpaceState::~SpaceState() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/configuration/SpaceState.h
@@ -0,0 +1,21 @@
+/*
+ * SpaceState.h
+ *
+ *  Created on: Jul 22, 2016
+ *      Author: belleau
+ */
+
+#ifndef SPACESTATE_H_
+#define SPACESTATE_H_
+
+namespace space_process{
+
+class SpaceState {
+public:
+	SpaceState();
+	virtual ~SpaceState();
+};
+
+}; /* namespace space_process */
+
+#endif /* SPACESTATE_H_ */
