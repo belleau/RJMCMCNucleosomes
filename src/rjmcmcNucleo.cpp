e0a2548d0f693fe80800c43fb186f5d9b879316f
belleau <pascal_belleau@hotmail.com>
2016-11-30 15:09:30 -0500

    Added gsl_set_error_handler_off

--- src/rjmcmcNucleo.cpp
+++ src/rjmcmcNucleo.cpp
@@ -42,13 +42,15 @@ List rjmcmcNucleo(SEXP startPosForwardReads, SEXP startPosReverseReads,
     const gsl_rng_type * T;
     gsl_rng *rng;
 
-
+    gsl_set_error_handler_off();
     T = gsl_rng_default;
 
     rng = gsl_rng_alloc (T);     // pick random number generator
+
     if(vSeed <= 0){
         vSeed = time (NULL) * getpid();
     }
+    gsl_set_error_handler_off();
     gsl_rng_set (rng, vSeed);
 
     /*********************************************************************
