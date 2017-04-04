1c8ba2e951c33f4bf4090f4f709b1b042f299b9d
adeschen <adeschen@hotmail.com>
2017-04-04 15:37:58 -0400

    Add unit tests

---
+++ inst/unitTests/test_print.rjmcmcNucleosomesBeforeAndAfterPostTreatment.R
@@ -0,0 +1,46 @@
+###################################################
+# Created by Astrid Deschenes
+# 2015-03-08
+###################################################
+
+########################################################################
+## Test the print.rjmcmcNucleosomesBeforeAndAfterPostTreatment function
+########################################################################
+
+### {{{ --- Test setup ---
+
+if(FALSE) {
+    library( "RUnit" )
+    library( "RJMCMCNucleosomes" )
+}
+
+### }}}
+
+data("reads_demo_01")
+
+####################################################################
+## print.rjmcmcNucleosomesBeforeAndAfterPostTreatment() function
+####################################################################
+
+test.print.rjmcmcNucleosomesBeforeAndAfterPostTreatment_returned_value <- function() {
+
+    ##directoryWithRDSFiles <- system.file("extdata", package = "RJMCMCNucleosomes")
+
+    ##resultInit <- mergeAllRDSFilesFromDirectory(directoryWithRDSFiles)
+
+    ##  forward <- start(reads_demo_01[strand(reads_demo_01) == "+"])
+    ## reverse <- end(reads_demo_01[strand(reads_demo_01) == "-"])
+    ## postResult <- postTreatment(startPosForwardReads = reads_demo$readsForward,
+    ##                             startPosReverseReads = reads_demo$readsReverse,
+    ##                             resultInit, 74, 73500)
+
+    ##result <- print(postResult)
+
+    ## message <- paste0(" test.print.rjmcmcNucleosomesBeforeAndAfterPostTreatment_returned_value() ",
+    ##                   "- print method did not returned expected value")
+
+    ##checkEquals(postResult, result, msg = message)
+
+    ## TODO
+    return(TRUE)
+}
